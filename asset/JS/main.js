f18c1438cc43ac81f242541752cd12c44899ca11
planeset94 <planeset94@gmail.com>
2021-04-28 15:48:52 +0200

    displaying array'selement in a list

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -4,6 +4,13 @@ var surname = prompt('Type yoour surname please');
 var list = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 list.push(surname);
 console.log(list);
-
 // 3. stampa la lista ordinata alfabeticamente
+var lista_ordinata = list.sort();
+console.log(lista_ordinata);
+
+for (var i = 0; i < lista_ordinata.length; i++) {
+    document.getElementById('stampa').innerHTML += "<li>" + lista_ordinata[i] + "</li>";
+
+}
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
